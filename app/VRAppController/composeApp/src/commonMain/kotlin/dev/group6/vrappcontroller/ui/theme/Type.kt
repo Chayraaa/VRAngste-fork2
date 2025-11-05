ec561c4c4d1206f035d74635e9bb536d49cae7b3
Karla <karla.schra@gmail.com>
2025-11-05 22:01:15 +0100

    I forgor

---
+++ app/VRAppController/composeApp/src/commonMain/kotlin/dev/group6/vrappcontroller/ui/theme/Type.kt
@@ -0,0 +1,20 @@
+package dev.group6.vrappcontroller.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val AppTypography = Typography(
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Monospace,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+    )
+)
