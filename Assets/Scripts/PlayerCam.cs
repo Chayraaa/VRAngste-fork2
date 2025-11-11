a4cbdb240f088cb3327fd22da0c2b52531a746ff
BenjaminLoch <bloch@hof-university.de>
2025-11-11 14:29:48 +0100

    first person eingerichtet

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
+using UnityEngine.XR.Management;
 
 public class PlayerCam : MonoBehaviour
 {
     public float sensX;
     public float sensY;
+    private bool disableXRInEditor = true;
 
     public Transform orientation;
 
     float xRotation;
     float yRotation;
 
-    private void Start(){
+    private void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    private void Update(){
+    private void Awake()
+    {
+#if UNITY_EDITOR
+        if (disableXRInEditor)
+        {
+            DisableXR();
+        }
+#endif
+    }
+
+    private void Update()
+    {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-    
+
         yRotation += mouseX;
 
         xRotation -= mouseY;
@@ -26,5 +40,22 @@ public class PlayerCam : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+    
+     private void DisableXR()
+    {
+        var xrManager = XRGeneralSettings.Instance?.Manager;
+        if (xrManager == null)
+        {
+            Debug.LogWarning("[DisableXRIfTesting] Kein XR Manager gefunden.");
+            return;
+        }
+
+        if (xrManager.isInitializationComplete)
+        {
+            xrManager.StopSubsystems();
+            xrManager.DeinitializeLoader();
+            Debug.Log("[DisableXRIfTesting] XR deaktiviert für Editor-Testlauf.");
+        }
     }
 }
